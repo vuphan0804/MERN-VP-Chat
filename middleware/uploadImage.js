c232379df40bd22a806ea0e0c30276c3ece4982e
vuphan0804 <vuphan0804@gmail.com>
2021-06-04 16:58:11 +0700

    Upload-Avatar

--- middleware/uploadImage.js
+++ middleware/uploadImage.js
@@ -4,15 +4,18 @@ module.exports = async (req, res, next) => {
     try {
         if(!req.files || Object.keys(req.files).length === 0)
             return res.status(400).json({msg: "No files were uploaded."})
-        const file = req.files.file
+            
+        const file = req.files.file;
 
-        console.log(file)
-
-        if(file.size > 1024*1024){
-            removeTmp(file.tempfilePath)
+        if(file.size > 1024 * 1024){
+            removeTmp(file.tempFilePath)
             return res.status(400).json({msg: "Size too large."})
+        } // 1mb
 
-        } //!Mb
+        if(file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
+            removeTmp(file.tempFilePath)
+            return res.status(400).json({msg: "File format is incorrect."})
+        }
 
         next()
     } catch (err) {
@@ -21,7 +24,7 @@ module.exports = async (req, res, next) => {
 }
 
 const removeTmp = (path) => {
-    fs.unlink(path, err =>{
+    fs.unlink(path, err => {
         if(err) throw err
     })
 }
