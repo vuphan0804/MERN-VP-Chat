52dd0ec425a884c3789438f829f8a7e814a811a0
vuphan0804 <vuphan0804@gmail.com>
2021-06-17 21:03:02 +0700

    Message UI Home

--- client/src/components/body/home/Home.jsx
+++ client/src/components/body/home/Home.jsx
@@ -1,8 +1,55 @@
 import React from "react";
+import Conversation from "./conversation/Conversation";
+import Message from "./message/Message";
+import ChatOnline from "./chatOnline/ChatOnline";
+
 import "./home.scss";
 
 function Home() {
-  return <div>VP Chat</div>;
+  return (
+    <div className="messenger">
+      {/* Menu */}
+      <div className="chatMenu">
+        <div className="chatMenuWrapper">
+          <input placeholder="Search for friends" className="chatMenuInput" />
+          <Conversation />
+          <Conversation />
+          <Conversation />
+          <Conversation />
+        </div>
+      </div>
+      {/* ChatBox */}
+      <div className="chatBox">
+        <div className="chatBoxWrapper">
+          <div className="chatBoxTop">
+            <Message />
+            <Message own={true} />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+          </div>
+          <div className="chatBoxBottom">
+            <textarea
+              className="chatMessageInput"
+              placeholder="write somthing..."
+            ></textarea>
+            <button className="chatSubmitButton">Send</button>
+          </div>
+        </div>
+      </div>
+      {/* Online */}
+      <div className="chatOnline">
+        <div className="chatOnlineWrapper">
+          <ChatOnline />
+          <ChatOnline />
+          <ChatOnline />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
